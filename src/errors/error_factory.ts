c948f99a349e8826f07b321cbb21691a6d62fcee
Frasan00 <francesco.sangiovanni.7@gmail.com>
2025-10-15 18:43:09 +0200

    chore: Bump version to 0.0.30 in package.json and remove error name from errorFactory

--- src/errors/error_factory.ts
+++ src/errors/error_factory.ts
@@ -2,7 +2,6 @@ import type { BaldaError } from "src/errors/balda_error";
 
 export const errorFactory = (error: BaldaError) => {
   return {
-    name: error.constructor.name,
     cause: error.cause,
     message: error.message,
     stack: error.stack,
