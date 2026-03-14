90b0edd51dc53b9d2c579c5b4718589c6bbea51b
francesco sangiovanni <francesco.sangiovanni.7@gmail.com>
2026-03-14 13:56:38 +0100

    Deploy website - based on 58a687949f6b4ed89c358f2d783049d331a1c91e

---
+++ assets/js/acecf23e.0cb1be3c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:a=>{a.exports=JSON.parse('{"blogBasePath":"/balda/blog","blogTitle":"Balda Blog","authorsListPath":"/balda/blog/authors"}')}}]);
